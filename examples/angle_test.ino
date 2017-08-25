d1394bf4ad36368724526a0e03e79cc3f32cf5f1
marcudanf <marcudanf@gmail.com>
2017-08-25 22:32:01 +0300

    Create angle_test.ino

---
+++ examples/angle_test.ino
@@ -0,0 +1,20 @@
+#include <gyroscope.h>
+
+Gyroscope gyroscope(2);
+
+Angle angle;
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  angle=gyroscope.getypr();
+  Serial.print("yaw=");
+  Serial.print(angle.yaw);
+  Serial.print("\tpitch=");
+  Serial.print(angle.pitch);
+  Serial.print("\troll");
+  Serial.println(angle.roll);
+}
